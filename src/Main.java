aa82140c48bc9a98c3cb7834e920e3b86e6ec0a8
Dariusk99 <dariuszkosnicki@gmail.com>
2026-06-03 17:44:30 +0200

    change file path

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Disk disk = new Disk("fat32.img");
+            Disk disk = new Disk("../fat32.img");
             Fat32 fat = new Fat32(disk);
 
             ConsoleMenu consoleMenu = new ConsoleMenu(fat);
